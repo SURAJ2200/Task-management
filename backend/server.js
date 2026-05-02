d2e94b49164751e117b8ca79c9b3bb78abedd3d8
SURAJ2200 <ujjawalmishra729@gmail.com>
2026-05-02 13:58:31 +0530

    fix app export issue

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,25 @@
 require("dotenv").config();
 
-const app = require("./src/app");
-const connectDb = require("./src/config/database");
+const express = require("express");
+const cors = require("cors");
 
-connectDb.connect();
+const app = express();
 
-const PORT = process.env.PORT || 8080;
+app.use(cors());
+app.use(express.json());
+
 
 app.get("/", (req, res) => {
-  res.send("Backend Working 🚀");
+  res.send("Backend Running 🚀");
 });
 
 app.get("/api/health", (req, res) => {
   res.json({ ok: true });
 });
+
+
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
