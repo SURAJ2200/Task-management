372ea100d59ca0f556c497c91eb1680d4f7edb0e
SURAJ2200 <ujjawalmishra729@gmail.com>
2026-05-02 13:16:23 +0530

    fix server startup flow

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,26 @@
 require("dotenv").config();
-const app = require("./src/app")
-const connectDb = require("./src/config/database")
 
-connectDb.connect();
-const PORT = process.env.PORT || 5000
+const app = require("./src/app");
+const connectDb = require("./src/config/database");
 
-app.get("/", (req, res) => {
-  res.send("API Running 🚀");
-});
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT} - VERSION 2.0`)
-})
+
+connectDb.connect()
+  .then(() => {
+    console.log("DB Connected");
+
+  
+    app.get("/", (req, res) => {
+      res.send("API Running 🚀");
+    });
+
+   
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  })
+  .catch((err) => {
+    console.error("DB Failed ", err);
+  });
