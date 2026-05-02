1962cc744441c71f758c27736a646946e1711678
SURAJ2200 <ujjawalmishra729@gmail.com>
2026-05-02 13:18:24 +0530

    fix railway binding

--- backend/server.js
+++ backend/server.js
@@ -3,24 +3,14 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDb = require("./src/config/database");
 
+connectDb.connect();
+
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("API Running 🚀");
+});
 
-connectDb.connect()
-  .then(() => {
-    console.log("DB Connected");
-
-  
-    app.get("/", (req, res) => {
-      res.send("API Running 🚀");
-    });
-
-   
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-
-  })
-  .catch((err) => {
-    console.error("DB Failed ", err);
-  });
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
