c344b29a5029d21678a74f8c815cf52bb8e5cb04
SURAJ2200 <ujjawalmishra729@gmail.com>
2026-05-02 13:12:31 +0530

    add root route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const connectDb = require("./src/config/database")
 connectDb.connect();
 const PORT = process.env.PORT || 5000
 
+app.get("/", (req, res) => {
+  res.send("API Running 🚀");
+});
+
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT} - VERSION 2.0`)
 })
